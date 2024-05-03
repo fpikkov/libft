0ef5973d9632af1fb5a558202ba091bcb20a27af
Fred Pikkov <fpikkov@gmail.com>
2024-05-03 18:03:13 +0300

    Add files via upload
    Fixed ft_lstmap. Additional errors found in strict mode.

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,32 +6,37 @@
 /*   By: fpikkov <fpikkov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 15:47:04 by fpikkov           #+#    #+#             */
-/*   Updated: 2024/05/02 18:18:08 by fpikkov          ###   ########.fr       */
+/*   Updated: 2024/05/03 18:00:07 by fpikkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static t_list	*ft_clearlist(t_list *lst, void (*del)(void *))
+{
+	ft_lstclear(&lst, del);
+	return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	struct s_list	*head;
 	struct s_list	*tail;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (NULL);
-	head = NULL;
-	tail = NULL;
+	head = ft_lstnew(f(lst->content));
+	if (head == NULL)
+		return (ft_clearlist(head, del));
+	tail = head;
+	lst = lst->next;
 	while (lst != NULL)
 	{
-		ft_lstadd_back(&head, ft_lstnew(f(lst->content)));
-		if (tail == NULL)
-		{
-			ft_lstclear(&head, *del);
-			return (NULL);
-		}
+		tail->next = ft_lstnew(f(lst->content));
+		if (tail->next == NULL)
+			return (ft_clearlist(head, del));
 		tail = tail->next;
 		lst = lst->next;
 	}
-	tail = NULL;
 	return (head);
 }
