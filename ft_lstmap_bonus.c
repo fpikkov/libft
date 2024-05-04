e5627b8f8cd98fe73421806ec45a385781e6cdb2
Fred Pikkov <fpikkov@gmail.com>
2024-05-04 15:37:21 +0300

    Add files via upload
    Fixed an issue in ft_lstmap where allocated content wouldn't get freed in case of node allocation failure.

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,35 +6,41 @@
 /*   By: fpikkov <fpikkov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 15:47:04 by fpikkov           #+#    #+#             */
-/*   Updated: 2024/05/03 18:00:07 by fpikkov          ###   ########.fr       */
+/*   Updated: 2024/05/04 15:33:53 by fpikkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list	*ft_clearlist(t_list *lst, void (*del)(void *))
+static t_list	*ft_clearlist(t_list **lst, void **cont, void (*del)(void *))
 {
-	ft_lstclear(&lst, del);
+	if (*cont != NULL)
+		del(*cont);
+	if (*lst != NULL)
+		ft_lstclear(lst, del);
 	return (NULL);
 }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	struct s_list	*head;
-	struct s_list	*tail;
+	t_list	*head;
+	t_list	*tail;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	head = ft_lstnew(f(lst->content));
+	content = f(lst->content);
+	head = ft_lstnew(content);
 	if (head == NULL)
-		return (ft_clearlist(head, del));
+		return (ft_clearlist(&head, &content, del));
 	tail = head;
 	lst = lst->next;
 	while (lst != NULL)
 	{
-		tail->next = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		tail->next = ft_lstnew(content);
 		if (tail->next == NULL)
-			return (ft_clearlist(head, del));
+			return (ft_clearlist(&head, &content, del));
 		tail = tail->next;
 		lst = lst->next;
 	}
