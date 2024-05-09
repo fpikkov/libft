8e11aefa67580eee839c1cf4e8eb4fdd5885bc77
Fred Pikkov <fpikkov@gmail.com>
2024-05-09 17:47:01 +0300

    Add files via upload
    FT_CALLOC: Matched the overflow check with the libc version of the function.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: fpikkov <fpikkov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:16:15 by fpikkov           #+#    #+#             */
-/*   Updated: 2024/04/25 16:55:18 by fpikkov          ###   ########.fr       */
+/*   Updated: 2024/05/09 17:39:22 by fpikkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	max;
 
+	max = 4611686014132420608;
+	if (size && (max / size) < count)
+		return (0);
 	ptr = (void *)malloc(count * size);
 	if (!ptr)
 		return (0);
